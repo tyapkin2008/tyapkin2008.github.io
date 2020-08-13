d59ef340870a24d232b370542db23b745e27dd05
tyapkin2008@yandex.ru <tyapkin2008@yandex.ru>
2020-08-13 11:47:50 +0300

    2

--- sw.js
+++ sw.js
@@ -16,6 +16,65 @@ limitations under the License.
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.5.0/workbox-sw.js');
 
+var cacheName = 'bspr_cache_2020_08_13';
+var filesToCache = [
+  "/",
+  // "/api/posts",
+  "/style/main.css",
+  // "/jquery/dist/jquery.min.js",
+  // "/handlebars/dist/handlebars.min.js",
+  // "/dist/templates.js",
+  // "/dist/main.js",
+  // "https://fonts.googleapis.com/css?family=Spinnaker",
+  // "https://fonts.googleapis.com/css?family=Inconsolata",
+  // "https://cdnjs.cloudflare.com/ajax/libs/prism/1.16.0/prism.min.js",
+  // "https://cdnjs.cloudflare.com/ajax/libs/prism/1.16.0/themes/prism.min.css"
+];
+
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('activate', function(e) {
+  console.log('[ServiceWorker] Activate');
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (key !== cacheName) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.open(cacheName).then(function(cache) {
+      return caches.match(event.request)
+      .then(function(response) {
+        if (event.request.cache === 'only-if-cached' && event.request.mode !== 'same-origin') {
+          return response;
+        }
+        var fetchPromise = fetch(event.request).then(function(networkResponse) {
+          cache.put(event.request, networkResponse.clone());
+          return networkResponse;
+        })
+        return response || fetchPromise;
+      })
+    })
+  );
+});
+
+
 if (workbox) {
   console.log(`Yay! Workbox is loaded 🎉`);
 
